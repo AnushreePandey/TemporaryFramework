7589a01cbb15280d17df03e7dfd410494d8e2820
AnushreePandey <anushree.pandey88@gmail.com>
2022-05-15 16:56:52 +0530

    first commit

---
+++ src/main/java/com/company/bdd/TestFramework/RestAPITest.java
@@ -0,0 +1,32 @@
+package com.company.bdd.TestFramework;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+@Test
+public void Test1() {
+	System.out.println("Test1");
+}
+
+@Test
+public void Test2() {
+	System.out.println("Test2");
+}
+
+@Test
+public void Test3() {
+	System.out.println("Test3");
+}
+
+@Test
+public void Test4() {
+	System.out.println("Test4");
+}
+
+@Test
+public void Test5() {
+	System.out.println("Test5");
+}
+
+}
